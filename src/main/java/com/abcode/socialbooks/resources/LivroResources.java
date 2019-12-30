d1edad48cac6790c6244ac27e4231261a8337895
Arthur Barbosa <arthurbruno03@gmail.com>
2019-12-29 22:38:14 -0400

    tratamento de resposta HTTP 404 e 201

--- src/main/java/com/abcode/socialbooks/resources/LivroResources.java
+++ src/main/java/com/abcode/socialbooks/resources/LivroResources.java
@@ -1,9 +1,12 @@
 package com.abcode.socialbooks.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.abcode.socialbooks.domain.Livro;
 import com.abcode.socialbooks.repository.LivroRepository;
@@ -29,20 +33,27 @@ public class LivroResources {
 	}
 
 	@PostMapping
-	public void salvar(@RequestBody Livro livro) {
-		livroRepository.save(livro);
+	public ResponseEntity<?> salvar(@RequestBody Livro livro) {
+		livro = livroRepository.save(livro);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(livro.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 
 	@GetMapping("/{id}")
-	public Livro buscar(@PathVariable Long id) {
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
 		Optional<Livro> obj = livroRepository.findById(id);
-		return obj.orElseThrow(null);
+		if(!obj.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(obj);
 	}
 
 	@DeleteMapping("/{id}")
 	public void deletar(@PathVariable Long id) {
-		Livro obj = this.buscar(id);
-		livroRepository.deleteById(obj.getId());
+		livroRepository.deleteById(id);
 	}
 
 	@PutMapping("/{id}")
