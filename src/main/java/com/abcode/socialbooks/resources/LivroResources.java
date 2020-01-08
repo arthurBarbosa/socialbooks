517daaec2999cedf4edf25a59bd013bc0bfcfb16
Arthur Barbosa <arthurbruno03@gmail.com>
2020-01-07 20:54:35 -0400

    liberando cross origin apenas para o metodo listar

--- src/main/java/com/abcode/socialbooks/resources/LivroResources.java
+++ src/main/java/com/abcode/socialbooks/resources/LivroResources.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ public class LivroResources {
 	@Autowired
 	private ComentarioService comentarioService;
 
+	@CrossOrigin
 	@GetMapping
 	public ResponseEntity<List<Livro>> listar() {
 		return ResponseEntity.status(HttpStatus.OK).body(livroService.listar());
